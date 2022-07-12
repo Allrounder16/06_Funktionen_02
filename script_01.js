07a4c65e28ab4280c4963e2bb1bc1f95db9dc10b
M.D.M <marco-demaria@gmx.de>
2022-07-12 11:12:16 +0200

    // module: console output

--- script_01.js
+++ script_01.js
@@ -1,7 +1,7 @@
 
 /*** Rechner */
 /*
-0. a+b / a-b/ a*b / a/b  // ergebnis c 
+0. AUFGABE: a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart :
 3. Fkt. Grundrechenarten :
@@ -9,4 +9,22 @@
 */
 
 
- 
+/*** Rechner */
+/*
+0. a+b / a-b/ a*b / a/b  // ergebnis c 
+1. Dateneingabe + -überprüfung :
+2. Auswahl Rechenart :
+3. Fkt. Grundrechenarten :
+4. Ausgabe in Konsole :check!
+*/
+
+
+
+
+// module: console output | test:
+// output("hello");
+// output(20);
+function output(inputData) {
+    console.log(inputData);
+ }
+
