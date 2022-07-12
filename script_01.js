769fbf344419a58eca107a503d085ac407762c4f
M.D.M <marco-demaria@gmx.de>
2022-07-12 12:26:11 +0200

    division durch 0

--- script_01.js
+++ script_01.js
@@ -1,3 +1,4 @@
+
 /*** Rechner */
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
@@ -7,12 +8,49 @@
 4. Ausgabe in Konsole :check!
 */
 
+// module: division a / b |  test:
+output(divide(2,4));
+output(divide(2,-3));
+output(divide(2,0));
+output(divide(0,2));
+function divide(a,b) {
+
+    if (b == 0) {
+        return "Division by 0 not possible!";
+    } 
+    
+    return a / b; 
+
+}
 
 
+// module: multiplication a * b |  test:
+// output(multiply(2,3));
+// output(multiply(2,-3));
+// output(multiply(2,0));
+function multiply(a,b) {
+    return a * b;
+}
+
+// module: subtraction a - b |  test:
+// output(subtract(2,3));
+// output(subtract(2,-3));
+// output(subtract(2,0));
+function subtract(a,b) {
+    return a - b;
+}
+
+// module: addition a + b |  test:
+// output(add(2,3));
+// output(add(2,-3));
+// output(add(2,0));
+function add(a,b) {
+    return a + b;
+}
 
 // module: console output | test:
 // output("hello");
 // output(20);
-function output(inputData) {
+ function output(inputData) {
     console.log(inputData);
  }
