92f646116503770ce845313b900b3926408fd0d9
M.D.M <marco-demaria@gmx.de>
2022-07-13 08:18:10 +0200

    TO DO:  output(20) ---> "The result is: "  20  output("Error") --> "Error"

--- script_01.js
+++ script_01.js
@@ -3,26 +3,67 @@
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart :
-3. Fkt. Grundrechenarten :
+2. Auswahl Rechenart : check!
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole :check!
 */
 
-// module: division a / b |  test:
-output(divide(2,4));
-output(divide(2,-3));
-output(divide(2,0));
-output(divide(0,2));
-function divide(a,b) {
+// application / App
+startApp();
+function startApp() {
+    output(calculator(getNumber("1"),getOp(),getNumber("2")));
+}
 
+
+function getNumber(numTxt) {
+    const displayStr = "Bitte Zahl " + numTxt + " eingeben:";
+    let inputStr = prompt(displayStr);
+    let num = parseInt(inputStr);
+    return num;
+}
+
+
+function getOp() {
+    const displayStr = "Bitte korrekten Operator (+ | - | * | :) eingeben";
+    let op = prompt(displayStr);
+    return op;
+}
+
+
+// module: calculator | tests:
+// agreement : "+","-","*",":"
+// output(calculator(2,2,"+"));
+// output(calculator(2,2,"-"));
+// output(calculator(2,2,"*"));
+// output(calculator(2,2,":"));
+// output(calculator(2,0,":"));
+// output(calculator(2,0,"#!"));
+function calculator(a,op,b) {
+    switch (op) {
+        case "+": // addition
+            return add(a,b);
+        case "-": // subtraction
+            return subtract(a,b);
+        case "*": // multiplication
+            return multiply(a,b);
+        case ":": // division
+            return divide(a,b);
+        default:
+            return "Something went wrong!";
+    }
+}
+
+// module: division a / b |  test:
+// output(divide(2,4));
+// output(divide(2,-3));
+// output(divide(2,0));
+// output(divide(0,2));
+function divide(a,b) {
     if (b == 0) {
         return "Division by 0 not possible!";
     } 
-    
     return a / b; 
-
 }
-
 
 // module: multiplication a * b |  test:
 // output(multiply(2,3));
@@ -54,3 +95,7 @@ function add(a,b) {
  function output(inputData) {
     console.log(inputData);
  }
+
+//  TO DO:
+//  output(20) ---> "The result is: "  20
+//  output("Error") --> "Error"
